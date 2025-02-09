dab8bb0020f98cc23edd3d956433a0271dc32a9b
Varsha Kotegar <varshakotegar26@gmail.com>
2025-02-09 22:29:54 +0530

    Objects and Arrays

---
+++ Lecture 44/index.js
@@ -0,0 +1,16 @@
+//Objects and Arrays
+let obj = {
+    name : "Varsha",
+    "full name": "Varsha Kotegar",
+    age: 19,
+    weight: "43kg",
+    height: "152cm",
+    greet: function(){
+        console.log("Hello jee kaise ho ");
+    }
+};
+
+console.log(obj);
+obj.greet();
+
+console.log(typeof(obj));
