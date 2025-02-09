a666d9a346be97c4a8ae5136efc9f40853336447
Varsha Kotegar <varshakotegar26@gmail.com>
2025-02-09 18:05:42 +0530

    Loops and String

--- Lecture 42/index.js
+++ Lecture 42/index.js
@@ -1,61 +1,61 @@
-// //Loops and String 
+//Loops and String 
 
-// // for(initialization; condition; updation);
-// let i;
-// for( i=10; i>0;i--){
-//     console.log(i);
-// }
-// //break
-// for(i=0;i<5;i++){
-//     if(i==3)
-//         break;
-//     console.log(i);
-// }
+// for(initialization; condition; updation);
+let i;
+for( i=10; i>0;i--){
+    console.log(i);
+}
+//break
+for(i=0;i<5;i++){
+    if(i==3)
+        break;
+    console.log(i);
+}
 
-// //continue
+//continue
 
-// for(i=0;i<=5;i++){
-//     if(i==3)
-//         continue;
-//     console.log(i);
-// }
+for(i=0;i<=5;i++){
+    if(i==3)
+        continue;
+    console.log(i);
+}
 
-// //while
+//while
 
-// //do-while
+//do-while
 
-// //string
+//string
 
-// let firstName = 'Varsha';
-// let lastName = 'Kotegar';
+let firstName = 'Varsha';
+let lastName = 'Kotegar';
 
-// let name = `I'll
-// be 
-// a 
-// greatest
-// person 
-// :)`;
+let name = `I'll
+be 
+a 
+greatest
+person 
+:)`;
 
-// console.log(name);
+console.log(name);
 
-// let myname = new String("Varsha Kotegar");
-// console.log(myname);
+let myname = new String("Varsha Kotegar");
+console.log(myname);
 
 
 //Operations
-// let op1 = ' Kannada';
-// let op2 = 'Kundapura';
+let op1 = ' Kannada';
+let op2 = 'Kundapura';
 
-// let ans = op2 + op1;      //Output : Kundapura Kannada
-// console.log(ans);
-// let finalAns = `${op1} + ${op2}`;      //Output :  Kannada + Kundapura
-// console.log(finalAns);
-// let fAns = `${op1}${op2}`;      //Output :  KannadaKundapura
-// console.log(fAns);
-// let finalAnswer = `${op1} and ${op2}`;      //Output : Kannada and Kundapura
-// console.log(finalAnswer);
-// console.log(finalAnswer.length);            //length
-// console.log(op1.toUpperCase());            //Upper
+let ans = op2 + op1;      //Output : Kundapura Kannada
+console.log(ans);
+let finalAns = `${op1} + ${op2}`;      //Output :  Kannada + Kundapura
+console.log(finalAns);
+let fAns = `${op1}${op2}`;      //Output :  KannadaKundapura
+console.log(fAns);
+let finalAnswer = `${op1} and ${op2}`;      //Output : Kannada and Kundapura
+console.log(finalAnswer);
+console.log(finalAnswer.length);            //length
+console.log(op1.toUpperCase());            //Upper
 
 
 let str = 'Varsha is an Ambitious Girl';
