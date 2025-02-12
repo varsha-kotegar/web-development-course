39670faeb1c9500e9744f1a5e0565b4b5bb2d5aa
Varsha Kotegar <varshakotegar26@gmail.com>
2025-02-12 12:08:41 +0530

    built-in

---
+++ Lecture 48/index.js
@@ -0,0 +1,15 @@
+console.log(Math.PI);
+console.log(Math.max(15,58,98,54,77,87,48,47,88,99,758,12,44,74,46));      //Output: 758
+console.log(Math.min(15,58,98,54,77,87,48,47,88,99,758,12,44,74,46));      //Output: 12
+
+console.log(Math.round(1.4));      //Output: 1
+console.log(Math.floor(1.6));      //Output: 1
+console.log(Math.ceil(1.6));      //Output: 2
+console.log(Math.abs(5));      //Output: 5
+console.log(Math.abs(-5));      //Output: 5
+console.log(Math.sqrt(625));
+console.log(Math.pow(2,4));
+
+
+let curDate = new Date();
+console.log(curDate);
