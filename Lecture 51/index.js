9735d6f842e457c32b675415e6c62b00fcf8acb9
Varsha Kotegar <varshakotegar26@gmail.com>
2025-02-13 18:36:54 +0530

    DOM Manipulation

---
+++ Lecture 51/index.js
@@ -0,0 +1,11 @@
+// JS DOM Manipulation - Access/Insert/Update/Delete elements
+
+// in console after inspect
+
+//access
+document.getElementById("fpara");
+document.getElementsByClassName("Mypara");
+document.getElementsByTagName("p");
+
+document.querySelector('#fpara');
+document.querySelectorAll('#fpara');
